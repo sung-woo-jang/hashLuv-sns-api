55c451fbe5a1fd2b3170150fab30b4c47a3b80e7
gyomdyung <seastory624@gmail.com>
2022-09-23 10:59:40 +0900

    Feat: Boards 엔티티 정의

--- src/api/boards/entities/board.entity.ts
+++ src/api/boards/entities/board.entity.ts
@@ -1,1 +1,18 @@
-export class Board {}
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Column, Entity } from 'typeorm';
+import { CommonEntity } from './../../../common/entities/common-entity';
+
+@Entity()
+export class Board extends CommonEntity {
+  @IsNotEmpty()
+  @Column({ comment: '게시글 제목입니다.' })
+  title: string;
+
+  @IsNotEmpty()
+  @Column({ comment: '게시글 내용입니다,' })
+  description: string;
+
+  @IsNumber()
+  @Column({ comment: '게시글 조회수 입니다.', default: 0 })
+  viewCount: number;
+}
