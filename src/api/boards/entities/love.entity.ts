cf1797e66f9519b59f5eb9ddfd9ac375d5ee290f
gyomdyung <seastory624@gmail.com>
2022-09-23 11:23:58 +0900

    Feat: love(좋아요) 엔티티 정의

--- src/api/boards/entities/love.entity.ts
+++ src/api/boards/entities/love.entity.ts
@@ -1,1 +1,11 @@
-export class Love {}
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Column, Entity } from 'typeorm';
+import { CommonEntity } from './../../../common/entities/common-entity';
+
+@Entity()
+export class Love extends CommonEntity {
+  @IsNumber()
+  @IsNotEmpty()
+  @Column({ default: 0 })
+  loveCount: number;
+}
