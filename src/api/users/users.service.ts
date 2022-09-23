b7043019c322a27aedb897acff2e89f3e6b55fd6
gyomdyung <seastory624@gmail.com>
2022-09-23 15:23:57 +0900

    Feat: User 등록 API Demo

--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -12,13 +17,25 @@ export class UsersService {
   ) {}
 
   async signUp(createUserDto: CreateUserDto) {
-    // TODO: validate Email
+    const { email, name, password } = createUserDto;
+    // 이메일 중복 검사
+    if (await this.userRepository.findOne({ where: { email } }))
+      throw new ConflictException(
+        `이미 가입한 이메일입니다. 다른 계정으로 회원가입 해주세요.`,
+      );
 
-    // TODO: 비밀번호 암호화
+    // 비밀번호 암호화
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    // TODO: 유저 등록
+    // 유저 등록
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .insert()
+      .into(User)
+      .values({ email, name, password: hashedPassword })
+      .execute();
 
-    // return : { success: true }
-    return '';
+    if (!user) throw new InternalServerErrorException();
   }
 }
