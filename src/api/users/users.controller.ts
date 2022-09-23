a10bd86f8b53a9c7cdd85aa35a98794cee5a7989
gyomdyung <seastory624@gmail.com>
2022-09-23 10:44:55 +0900

    Docs: User API 수도코드 작성

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -4,4 +4,25 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  /*
+  return {}
+  
+  @Post('signup')
+  singup(){
+    TODO:조건 1
+    
+    TODO:조건 2
+  }
+  */
+
+  /* 
+  return {}
+  @Post('login')
+  login(){
+TODO: 이메일 비밀번호로 사용자 인증
+
+TODO: JWT토큰 발급
+  }
+  */
 }
