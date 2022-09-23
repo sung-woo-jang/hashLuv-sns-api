08fc746e7fc31380c0bf029315757a4f9d85064b
gyomdyung <seastory624@gmail.com>
2022-09-23 10:56:10 +0900

    Feat: User 엔티티 정의

--- src/api/users/entities/user.entity.ts
+++ src/api/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { CommonEntity } from './../../../common/entities/common-entity';
+import { Column, Entity } from 'typeorm';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+@Entity()
+export class User extends CommonEntity {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @Column()
+  email: string;
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @Matches(/^(?=.*?[0-9]).{6,}$/, {
+    message:
+      '비밀번호는 6자 이상이어야 하고, 숫자 1개 이상 반드시 포함 되어야 합니다.',
+  })
+  password: string;
+}
--- src/api/users/users.module.ts
+++ src/api/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
